7de335beb11130b58d843f29beee803a399818f6
yuyamitsu <92497779+yuyamitsu@users.noreply.github.com>
2025-08-27 15:51:04 +0900

    ドロワー編集

--- works/games/includes/header.php
+++ works/games/includes/header.php
@@ -15,8 +15,8 @@
         <li><button class="addNameBtn" onclick="addName()">名前を登録・変更する</button></li>
         <li><button class="deleteNameBtn" onclick="deleteName()">名前を削除する</button></li>
         <li>スコアを初期化する</li>
-        <li>＊ご利用のブラウザに保存されます</li>
       </ul>
+      <p class="nameInfo">*ご利用のブラウザに保存されます</p>
     </div>
   </header>
   <script src="javascript/header.js"></script>
